027d6e444f7e18b0977fb6fea468faa855eaca67
Prabin Magar <psmagar123451@gmail.com>
2022-11-16 11:44:10 +0545

    source code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
     //     autoplay: true,
     //     autoplaySpeed: 3000,
     // });
+
+    let resizeTimer;
+    window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+    });
     
     $('.msg-slider').slick({
         dots: false,
@@ -18,6 +27,39 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 3000,
     });
+
+    $('.clients-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    })
 
 
     $('.navbar-show-btn').click(() => {
